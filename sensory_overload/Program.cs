98136990fa7ea4ff6eab13fb566fd481fbb74ebe
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-29 10:49:35 +0200

    w

---
+++ sensory_overload/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace sensory_overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Worite();
+            Worite();
+        }
+        //static void Worite()
+        //{
+        //    Console.WriteLine("Hei og velkommen");
+        //}
+        static void Worite(string name = "Terje")
+        {
+            Console.WriteLine("Hei og velkommen. " + name);
+        }
+    }
+}
